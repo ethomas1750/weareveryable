000b7cf79844f420e72b140d163eb5e94cc85aa8
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-05 23:17:03 -0500

    Create main .js

---
+++ main .js
@@ -0,0 +1,81 @@
+/******************
+ * YOUR CODE BELOW *
+ ******************/
+let myFirstName = "Edwin"
+let myLastName = "Thomas Jr."
+let myFavoriteCeleb = "Michael Jai Wjite"
+let mySentence = "My name is: " + myFirstName + " " + myLastName + " and my favorite celeb is " + myFavoriteCeleb
+let myLuckyNum = 7
+let myAge = 20
+let myHeight = 43
+let myFavoriteYear = 2021
+let mySum = 5 + 2317 + 36
+let myProduct = 232 * 43 / 2 + 4
+let myHeightAndAge = myAge + myHeight
+
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof myFirstName === "undefined") {
+  myFirstName = undefined;
+}
+
+if (typeof myLastName === "undefined") {
+  myLastName = undefined;
+}
+
+if (typeof myFavoriteCeleb === "undefined") {
+  myFavoriteCeleb = undefined;
+}
+
+if (typeof mySentence === "undefined") {
+  mySentence = undefined;
+}
+
+if (typeof myLuckyNum === "undefined") {
+  myLuckyNum = undefined;
+}
+
+if (typeof myAge === "undefined") {
+  myAge = undefined;
+}
+
+if (typeof myHeight === "undefined") {
+  myHeight = undefined;
+}
+
+if (typeof myFavoriteYear === "undefined") {
+  myFavoriteYear = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof myProduct === "undefined") {
+  myProduct = undefined;
+}
+
+if (typeof myHeightAndAge === "undefined") {
+  myHeightAndAge = undefined;
+}
+
+module.exports = {
+  myFirstName,
+  myLastName,
+  myFavoriteCeleb,
+  mySentence,
+  myLuckyNum,
+  myAge,
+  myHeight,
+  myFavoriteYear,
+  mySum,
+  myProduct,
+  myHeightAndAge,
+};
